1b9ced6e5e3e053d9358bf4480c06352549655a7
hibersh <hibersh@gmail.com>
2011-07-18 13:19:45 +0800

    Initial Commit

---
+++ cef.js
@@ -0,0 +1,16 @@
+(function ($) {
+  
+Drupal.behaviors.cef = {
+  attach: function (context) {
+    $.each(Drupal.settings.cef, function(dependee, dependents) {
+      $('[name="'+dependee+'"]').change(function() {
+        var value = this.value;
+        $.each(dependents, function(dependent, dvalue) {
+          $('[name="'+dependent+'"]').closest('.views-exposed-widget').toggle(dvalue == value);
+        });
+      });
+    });
+  }
+}
+
+})(jQuery);
