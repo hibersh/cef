74c47ae36136e97d845c05202cc230677b4ae7d8
Hiber <hiber@hibersh.com>
2011-10-02 09:18:01 +0800

    fix toggle error

--- cef.js
+++ cef.js
@@ -4,7 +4,12 @@ Drupal.behaviors.cef = function (context) {
         $(this).closest('.views-exposed-widget').addClass('dependee');
         var value = this.value;
         $.each(dependents, function(dependent, values) {
-          $('[name="'+dependent+'"]').closest('.views-exposed-widget').toggle(value in values).addClass('dependent');
+          $('[name="'+dependent+'"]').closest('.views-exposed-widget').addClass('dependent');
+          if (value in values) {
+            $('[name="'+dependent+'"]').closest('.views-exposed-widget').show();
+          } else {
+            $('[name="'+dependent+'"]').closest('.views-exposed-widget').hide();
+          }
         });
       });
       $('[name="'+dependee+'"]').change();
@@ -13,7 +18,10 @@ Drupal.behaviors.cef = function (context) {
     $('.dependent').wrapAll('<div class="views-exposed-widgets dependent" />');
     $('.views-exposed-widget:not(.dependee,.dependent)').wrapAll('<div class="views-exposed-widgets other" />');
     $.each($('.views-exposed-widget .form-select'), function() {
-      $(this).get(0).options[0].text = $(this).closest('.views-exposed-widget').children('label').text();
-      $(this).closest('.views-exposed-widget').children('label').remove();
+      if ($("label[for='"+this.id+"']").text()) {
+        this.options[0].text = $("label[for='"+this.id+"']").text();
+        $("label[for='"+this.id+"']").remove();        
+      }
     });
-}
+};
+
